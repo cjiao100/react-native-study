f51fde051f1be2875c2fe6ddf7cb4c3f2d1ca639
cjiao100 <1971416012@qq.com>
2019-11-20 21:56:33 +0800

    使用fetch请求

--- App.js
+++ App.js
@@ -1,53 +1,46 @@
 import React, { Component } from 'react';
-import { FlatList, SectionList, StyleSheet, Text, View } from 'react-native';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
 
 /**
- * 长列表
- * 1. FaltList 必须要有data（列表数据源）和renderItem（渲染方式）两个属性
- * 2. SectionList 比较适合拥有分组的列表 sections（数据源）renderItem（设置渲染模板）renderSectionHeader（设置头部的渲染模板）renderSectionFooter（设置底部的渲染模板）keyExtractor（设置key）
+ * 网络
+ * 1. fetch API
  */
 
 export default class FlatListBasics extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { dataSource: [] };
+  }
+
+  componentDidMount() {
+    this.getMoviesFromApiAsync();
+  }
+
+  getMoviesFromApiAsync() {
+    return fetch('https://facebook.github.io/react-native/movies.json')
+      .then(response => response.json())
+      .then(responseJson => {
+        console.log(responseJson.movies);
+        this.setState({
+          dataSource: responseJson.movies
+        });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
   render() {
     return (
       <View>
         <FlatList
-          data={[
-            { key: 'Devin' },
-            { key: 'Dan' },
-            { key: 'Dominic' },
-            { key: 'Jackson' },
-            { key: 'James' },
-            { key: 'Joel' },
-            { key: 'John' },
-            { key: 'Jillian' },
-            { key: 'Jimmy' },
-            { key: 'Julie' }
-          ]}
-          renderItem={({ item }) => <Text style={styles.item}>{item.key}</Text>}
-        />
-        <View style={styles.block} />
-        <SectionList
-          sections={[
-            { title: 'D', data: ['Devin', 'Dan', 'Dominic'] },
-            {
-              title: 'J',
-              data: [
-                'Jackson',
-                'James',
-                'Jillian',
-                'Jimmy',
-                'Joel',
-                'John',
-                'Julie'
-              ]
-            }
-          ]}
-          renderItem={({ item }) => <Text style={styles.item}>{item}</Text>}
-          renderSectionHeader={({ section }) => (
-            <Text style={styles.sectionHeader}>{section.title}</Text>
+          data={this.state.dataSource}
+          renderItem={({ item }) => (
+            <Text style={styles.item}>
+              {item.title}---{item.releaseYear}
+            </Text>
           )}
-          keyExtractor={(item, index) => index}
+          keyExtractor={(item, index) => item.id}
         />
       </View>
     );
@@ -63,18 +56,5 @@ const styles = StyleSheet.create({
     padding: 10,
     fontSize: 18,
     height: 44
-  },
-  block: {
-    backgroundColor: '#000',
-    height: 10
-  },
-  sectionHeader: {
-    paddingTop: 2,
-    paddingLeft: 10,
-    paddingRight: 10,
-    paddingBottom: 2,
-    fontSize: 14,
-    fontWeight: 'bold',
-    backgroundColor: 'rgba(247,247,247,1.0)'
   }
 });
